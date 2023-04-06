5c49525adfa53e09ee7d4c0638314b709561cb6f
Kalil Georges Balech <kalilgb152@gmail.com>
2023-04-06 00:05:42 -0300

    user interface and song effects added

--- game.py
+++ game.py
@@ -3,21 +3,25 @@ import pygame
 
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 900
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
 CHARACTER_HEIGHT = 72
 CHARACTER_WIDTH = 58
 
 PLATFORM_HEIGHT = 50
 
-SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+WHITE = (255, 255, 255)
 DARK_GREY = (50,50,50)
 MUSTARD = (209,206,25)
+LIGHT_BLUE = (15, 235, 255)
+BLACK = (0, 0, 0)
 
 # init
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('De volta para o lar')
 clock = pygame.time.Clock()
+font = pygame.font.Font(pygame.font.get_default_font(), 24)
 
 
 #player
@@ -32,6 +36,13 @@ player_y_acelleration = 0.4
 #ground
 platforms = [pygame.Rect(0, SCREEN_HEIGHT-PLATFORM_HEIGHT, SCREEN_WIDTH, PLATFORM_HEIGHT),
              pygame.Rect(0, 0, SCREEN_WIDTH, 1)]
+
+# background song
+music = pygame.mixer.music.load('songs/galinha.ogg')
+pygame.mixer.music.play(-1)
+
+# coin sound
+coin_sound = pygame.mixer.Sound('songs/coin.ogg')
 
 # coins
 coin_image = pygame.image.load('images/coin_0.png')
@@ -56,11 +67,33 @@ coins = [
             pygame.Rect(4300, 300, 32, 32),
             pygame.Rect(3700, 300, 32, 32),
             pygame.Rect(4000, 400, 32, 32),
+
+            pygame.Rect(5000, 450, 32, 32),
+            pygame.Rect(5200, 400, 32, 32),
+            pygame.Rect(5400, 350, 32, 32),
+            pygame.Rect(5600, 300, 32, 32),
+            pygame.Rect(5800, 250, 32, 32),
+
+            pygame.Rect(7000, 450, 32, 32),
+            pygame.Rect(5200, 400, 32, 32),
+            pygame.Rect(5400, 350, 32, 32),
+            pygame.Rect(5600, 300, 32, 32),
+            pygame.Rect(5800, 250, 32, 32),
+            
+            pygame.Rect(8000, 200, 32, 32),
+            pygame.Rect(8050, 200, 32, 32),
+            pygame.Rect(8100, 200, 32, 32),
+            pygame.Rect(8150, 200, 32, 32),
+            pygame.Rect(8200, 200, 32, 32),
+            pygame.Rect(8250, 200, 32, 32),
+            pygame.Rect(8300, 200, 32, 32),
+            pygame.Rect(8350, 200, 32, 32),
+            pygame.Rect(8400, 200, 32, 32),
+            pygame.Rect(8450, 200, 32, 32)
         ]
 
 coin_speed = 5
 coins_collected = 0
-
 
 running = True
 while running:
@@ -72,7 +105,6 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
 
     # player input
     keys = pygame.key.get_pressed()
@@ -110,17 +142,18 @@ while running:
         if c.colliderect(player_rect):
             coins.remove(c)
             coins_collected += 1
+            coin_sound.play()
 
     # update
 
     # draw
 
     #background
-    screen.fill(DARK_GREY)
+    screen.fill(LIGHT_BLUE)
 
     #platforms
     for p in platforms:
-        pygame.draw.rect(screen, MUSTARD, p)
+        pygame.draw.rect(screen, BLACK, p)
 
     # coins
     for c in coins:
@@ -129,6 +162,16 @@ while running:
         screen.blit(coin_image, (c[0], c[1]))
 
     screen.blit(player_image, (player_x_position, player_y_position))
+
+    # Player information display
+
+    #coins
+    screen.blit(coin_image, (20, 20))
+    coin_text = font.render(str(coins_collected), True, BLACK, LIGHT_BLUE)
+    coin_text_rectangle = coin_text.get_rect()
+    coin_text_rectangle.topleft = (55, 25)
+    screen.blit(coin_text, coin_text_rectangle)
+    
     pygame.display.flip()
     clock.tick(60)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-player_x_position
+mus
